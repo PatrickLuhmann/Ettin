f5f7526ad4ad11f208614f8d89668f9ee3581987
Patrick Luhmann <patrick.luhmann@gmail.com>
2016-10-19 15:23:32 -0700

    Initial commit
    Basic controls are in place to create a table and add columns.
    No save button yet.
    
    Signed-off-by: Patrick Luhmann <patrick.luhmann@gmail.com>

---
+++ Ettin/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Ettin
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.buttonNewDataSet = new System.Windows.Forms.Button();
+			this.buttonNewDataTable = new System.Windows.Forms.Button();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.buttonNewColumn = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// buttonNewDataSet
+			// 
+			this.buttonNewDataSet.Location = new System.Drawing.Point(13, 13);
+			this.buttonNewDataSet.Name = "buttonNewDataSet";
+			this.buttonNewDataSet.Size = new System.Drawing.Size(108, 23);
+			this.buttonNewDataSet.TabIndex = 0;
+			this.buttonNewDataSet.Text = "New DataSet";
+			this.buttonNewDataSet.UseVisualStyleBackColor = true;
+			this.buttonNewDataSet.Click += new System.EventHandler(this.buttonNewDataSet_Click);
+			// 
+			// buttonNewDataTable
+			// 
+			this.buttonNewDataTable.Location = new System.Drawing.Point(128, 13);
+			this.buttonNewDataTable.Name = "buttonNewDataTable";
+			this.buttonNewDataTable.Size = new System.Drawing.Size(113, 23);
+			this.buttonNewDataTable.TabIndex = 1;
+			this.buttonNewDataTable.Text = "New DataTable";
+			this.buttonNewDataTable.UseVisualStyleBackColor = true;
+			this.buttonNewDataTable.Click += new System.EventHandler(this.buttonNewDataTable_Click);
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Location = new System.Drawing.Point(13, 43);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.Size = new System.Drawing.Size(1028, 840);
+			this.dataGridView1.TabIndex = 2;
+			// 
+			// buttonNewColumn
+			// 
+			this.buttonNewColumn.Location = new System.Drawing.Point(248, 13);
+			this.buttonNewColumn.Name = "buttonNewColumn";
+			this.buttonNewColumn.Size = new System.Drawing.Size(75, 23);
+			this.buttonNewColumn.TabIndex = 3;
+			this.buttonNewColumn.Text = "New Column";
+			this.buttonNewColumn.UseVisualStyleBackColor = true;
+			this.buttonNewColumn.Click += new System.EventHandler(this.buttonNewColumn_Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(330, 13);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(211, 20);
+			this.textBox1.TabIndex = 4;
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Items.AddRange(new object[] {
+            "int",
+            "unsigned int",
+            "string",
+            "DateTime",
+            "something else"});
+			this.comboBox1.Location = new System.Drawing.Point(548, 13);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(121, 21);
+			this.comboBox1.TabIndex = 5;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1054, 897);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.buttonNewColumn);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.buttonNewDataTable);
+			this.Controls.Add(this.buttonNewDataSet);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button buttonNewDataSet;
+		private System.Windows.Forms.Button buttonNewDataTable;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.Button buttonNewColumn;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.ComboBox comboBox1;
+	}
+}
+
---
+++ Ettin/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ettin
+{
+	public partial class Form1 : Form
+	{
+		DataSet CurrDataSet;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void buttonNewDataTable_Click(object sender, EventArgs e)
+		{
+			System.Diagnostics.Debug.WriteLine("Button: New DataTable");
+
+			if (CurrDataSet == null)
+			{
+				MessageBox.Show("ERROR: No DataSet present.");
+				return;
+			}
+
+			// Ask the user for a name for the table.
+			// For now, just take it from textBox1.
+			if (textBox1.Text == "")
+			{
+				MessageBox.Show("ERROR: Must supply name for new DataTable.");
+				return;
+			}
+
+			// Create the table and add it to the DataSet.
+			DataTable newDataTable = new DataTable(textBox1.Text);
+			// pjl Add debug column
+			newDataTable.Columns.Add("ID", typeof(UInt32));
+			newDataTable.PrimaryKey = new DataColumn[] { newDataTable.Columns["ID"] };
+			newDataTable.Columns["ID"].AutoIncrement = true;
+			newDataTable.Columns["ID"].AutoIncrementSeed = 1;
+			newDataTable.Columns["ID"].AutoIncrementStep = 1;
+
+			CurrDataSet.Tables.Add(newDataTable);
+
+			// Display the new table.
+			dataGridView1.DataSource = newDataTable;
+			//dataGridView1.Columns["testCol"].Visible = true;
+		}
+
+		private void buttonNewDataSet_Click(object sender, EventArgs e)
+		{
+			CurrDataSet = new DataSet("MyDataSet");
+		}
+
+		private void buttonNewColumn_Click(object sender, EventArgs e)
+		{
+			if (textBox1.Text == "")
+			{
+				MessageBox.Show("ERROR: Must supply name for new DataColumn.");
+				return;
+			}
+
+			// How do we know which DataTable is currently displayed?
+			// Can we add to the grid version and have it change the one in CurrDataSet?
+			// I guess there is only one way to find out.
+			DataTable dt = (DataTable)dataGridView1.DataSource;
+			dt.Columns.Add(textBox1.Text, typeof(int));
+		}
+	}
+}
